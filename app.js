f61e92d7bceb5dfa6de0038ae3155d4907eb4f08
rohitPandey469 <rd8614196@gmail.com>
2023-03-06 12:30:11 +0530

    commited

---
+++ app.js
@@ -0,0 +1,10 @@
+const twitter=document.querySelector("#twitter");
+twitter.addEventListener("click",()=>{
+    alert("Twitter is DEACTIVATED for the moment being!!!")
+});
+const btn=document.querySelector("#submit-btn");
+btn.addEventListener("click",()=>{
+    alert("Haven't connected with express or back end stuff, it won't submit the form manually");
+    alert("However if wants to connect, use rohitpandey73551@gmail.com");
+});
+
