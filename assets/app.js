130b4350dbc1c2d7f70ef92ae281d0f84d5e47f9
rohitPandey469 <rd8614196@gmail.com>
2023-03-06 12:38:39 +0530

    committed

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,12 @@
+const twitter=document.querySelector("#twitter");
+twitter.addEventListener("click",()=>{
+    alert("Twitter is DEACTIVATED for the moment being!!!")
+});
+const btn=document.querySelector("#submit-btn");
+btn.addEventListener("click",()=>{
+    alert("Haven't connected with express or back end stuff, it won't submit the form manually");
+    alert("However if wants to connect, use rohitpandey73551@gmail.com");
+});
 const scrollUp=document.querySelector("#scroll-up");
 scrollUp.addEventListener("click",()=>{
     window.scrollTo({
